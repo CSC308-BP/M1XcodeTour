c6dac12c0ed362a8f458eca9aa2b2b484efd682b
Brayden Peeples <braydenpeeples@gmail.com>
2026-01-22 13:03:47 -0500

    Created basic UI application.

--- M1XcodeTour/ViewController.swift
+++ M1XcodeTour/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+
+    @IBAction func button(_ sender: Any) {
+        let name = textField.text!
+        label.text = "Hello, \(name)!"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
